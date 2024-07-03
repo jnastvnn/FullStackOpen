f9ae7cda547f01a7f08183a3e73e60d9a5a7667f
jnastvnn <98950339+jnastvnn@users.noreply.github.com>
2024-07-03 15:52:22 +0300

    Finish phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -6,102 +6,89 @@ const App = () => {
     { name: 'Ada Lovelace', number: '39-44-5323523' },
     { name: 'Dan Abramov', number: '12-43-234345' },
     { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [filter, setFilter] = useState('')
+  ]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
 
-
-  const Persons=({persons}) => {return(
+  const Persons = ({ persons }) => (
     <ul>
-    {persons.map((person) => (
+      {persons.map((person) => (
         <li key={person.number}>{person.name} {person.number}</li>
       ))}
     </ul>
-  )}
+  );
 
+  const addPerson = (event) => {
+    event.preventDefault();
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to the phonebook`);
+      return;
+    }
+    const personObject = {
+      name: newName,
+      number: newNumber
+    };
+    setPersons(persons.concat(personObject));
+    setNewName('');
+    setNewNumber('');
+  };
 
-  const PersonForm = (props) =>(
+  const handleNameChange = (event) => {
     
-      <form onSubmit={props.onSubmit}>
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const personsToShow = filter
+    ? persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+    : persons;
+
+  const PersonForm = ({ addPerson, newName, handleNameChange, newNumber, handleNumberChange }) => (
+    <form onSubmit={addPerson}>
       <div>
-        name: <input 
-        value={props.newName}
-        onChange={props.handlePersonChange}/>
+        name: <input
+          value={newName}
+          onChange={handleNameChange}
+        />
       </div>
       <div>
         number: <input
-        value={props.newNumber}
-        onChange={props.handleNumberChange}
-        /></div>
-      
+          value={newNumber}
+          onChange={handleNumberChange}
+        />
+      </div>
       <div>
         <button type="submit">add</button>
       </div>
     </form>
-    
-  )
+  );
 
-  
-
-  const addPerson = (event) => {
-    event.preventDefault()
-    if (persons.some(person => person.name === newName)) {
-      alert(`${newName} is already added to the phonebook`);
-      return
-    }
-    const personObject = {
-      name: newName,
-      number: newNumber}
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
-  }
-  const handlePersonChange = (event) => {console.log(event.target.value)  
-    setNewName(event.target.value)  }
-  const handleNumberChange = (event) => {console.log(event.target.value)  
-    setNewNumber(event.target.value)  }
-  const handleFilterChange = (event) => {console.log(event.target.value)  
-    setFilter(event.target.value)  }
-
-  const personsToShow = filter
-  ? persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
-  : persons
-  
   return (
     <div>
       <h2>Phonebook</h2>
       <div>
         filter shown with: <input value={filter} onChange={handleFilterChange} />
       </div>
-           <form onSubmit={addPerson}>
-      <div>
-        name: <input 
-        value={newName}
-        onChange={handlePersonChange}/>
-      </div>
-      <div>
-        number: <input
-        value={newNumber}
-        onChange={handleNumberChange}
-        /></div>
-      
-      <div>
-        <button type="submit">add</button>
-      </div>
-    </form>
       <h3>add a new</h3>
-      <PersonForm 
-        onSubmit={addPerson}
+      <PersonForm
+        addPerson={addPerson}
         newName={newName}
-        handlePersonChange={handlePersonChange}
+        handleNameChange={handleNameChange}
         newNumber={newNumber}
         handleNumberChange={handleNumberChange}
       />
       <h2>Numbers</h2>
-      
+      <Persons persons={personsToShow} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
